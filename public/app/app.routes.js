486062a1f1150f5432032caa31d7b6203a9289cc
Jonathan Wrona <jonwrona@gmail.com>
2015-04-20 12:50:09 -0400

    made change to 404 and route to page. it is now a template instead of its own html page.

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -1,16 +1,18 @@
 angular.module('app.routes', ['ngRoute'])
-.config(function($routeProvider, $locationProvider) {
-	$routeProvider
-	// home page
-	.when('/', {
-		templateUrl: 'app/views/pages/home.html'
-	})
-	// spellbook page
-	.when('/spellbook', {
-		templateUrl: 'app/views/pages/spellbook.html',
-		controller: 'spellController',
-		controllerAs: 'spellCtrl'
-	}).
-	otherwise({redirectTo: 'app/views/pages/404.html'})
-	$locationProvider.html5Mode(true);
-});
+    .config(function($routeProvider, $locationProvider) {
+        $routeProvider
+        // home page
+            .when('/', {
+                templateUrl: 'app/views/pages/home.html'
+            })
+            // spellbook page
+            .when('/spellbook', {
+                templateUrl: 'app/views/pages/spellbook.html',
+                controller: 'spellController',
+                controllerAs: 'spellCtrl'
+            })
+            .otherwise({
+                templateUrl: 'app/views/pages/404.html'
+            });
+        $locationProvider.html5Mode(true);
+    });
